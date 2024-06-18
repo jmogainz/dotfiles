3879a81f83c46155ee112f7cc3e5a85cf2fdc2c6
jmogainz <private>
2024-06-18 11:35:07 -0500

    Disabled copilot, uncluttered lualine bar

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -221,12 +221,12 @@ ins_right {
   color = { fg = colors.green, gui = 'bold' },
 }
 
-ins_right {
-  'fileformat',
-  fmt = string.upper,
-  icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
-  color = { fg = colors.green, gui = 'bold' },
-}
+-- ins_right {
+--   'fileformat',
+--   fmt = string.upper,
+--   icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
+--   color = { fg = colors.green, gui = 'bold' },
+-- }
 
 ins_right {
   'filetype',
@@ -234,11 +234,11 @@ ins_right {
   color = { fg = colors.blue, gui = 'bold' },
 }
 
-ins_right {
-  'branch',
-  icon = '',
-  color = { fg = colors.violet, gui = 'bold' },
-}
+-- ins_right {
+--   'branch',
+--   icon = '',
+--   color = { fg = colors.violet, gui = 'bold' },
+-- }
 
 ins_right {
   'diff',
