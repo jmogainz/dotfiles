140b52e610522e7614d0a4b61afece7d30a27ffa
Jacob Moore <jmoore2@torchtechnologies.com>
2024-06-26 17:54:04 -0500

    update

--- .config/i3/findmouse.sh
+++ .config/i3/findmouse.sh
@@ -6,7 +6,7 @@ DEVICE_ID_ALT1=$(xinput list | grep "Logitech USB Receiver Mouse" | sed -n "s/.*
 if [ -n "$DEVICE_ID" ]; then
     xinput --set-prop "$DEVICE_ID" "libinput Accel Speed" -0.7
 elif [ -n "$DEVICE_ID_ALT1" ]; then
-    xinput --set-prop "$DEVICE_ID_ALT1" "libinput Accel Speed" -0.7
+    xinput --set-prop "$DEVICE_ID_ALT1" "libinput Accel Speed" -0.5
 else
     echo "Mouse not found"
 fi
