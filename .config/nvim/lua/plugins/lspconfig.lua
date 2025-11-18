41c900400f86ae093539822f14aa9f8b119ade4f
jmogainz <private>
2025-11-18 11:36:40 -0600

    update: lspconfig not deprecated anymore

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,83 +1,103 @@
+-- Capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 capabilities.offsetEncoding = { "utf-16" }
 
-local lspconfig = require('lspconfig')
-
 -- Shared on_attach function
 local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local opts = { noremap=true, silent=true }
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
 
-    -- Existing LSP mappings
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', 'gs', '<Cmd>ClangdSwitchSourceHeader<CR>', opts)
-    buf_set_keymap('n', 'gA', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', '<Leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<Leader>f', '<Cmd>lua vim.lsp.buf.format({ async = true })<CR>', opts)
+  local opts = { noremap = true, silent = true }
+
+  -- Existing Language Server Protocol (LSP) mappings
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', 'gs', '<Cmd>ClangdSwitchSourceHeader<CR>', opts)
+  buf_set_keymap('n', 'gA', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', '<Leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<Leader>f', '<Cmd>lua vim.lsp.buf.format({ async = true })<CR>', opts)
 end
 
-lspconfig.clangd.setup{
-    cmd = { "clangd", "--header-insertion=never", "--completion-style=detailed", "--cross-file-rename", "--clang-tidy" },
-    filetypes = { "c", "cpp", "objc", "objcpp" },
-    capabilities = capabilities,
-    on_attach = on_attach,
-}
+-----------------------------------------------------------------------
+-- Do NOT: local lspconfig = require('lspconfig')
+-- Do NOT: local lspconfig = vim.lsp.config
+-- Use vim.lsp.config + vim.lsp.enable instead
+-----------------------------------------------------------------------
 
-lspconfig.pyright.setup{
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        python = {
-            analysis = {
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = true,
-                typeCheckingMode = "standard",
-            },
-        },
-    },
-}
+-- Clangd
+vim.lsp.config('clangd', {
+  cmd = {
+    "clangd",
+    "--header-insertion=never",
+    "--completion-style=detailed",
+    "--cross-file-rename",
+    "--clang-tidy",
+  },
+  filetypes = { "c", "cpp", "objc", "objcpp" },
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
 
-lspconfig.lua_ls.setup {
+-- Pyright
+vim.lsp.config('pyright', {
+  capabilities = capabilities,
+  on_attach = on_attach,
   settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false,  -- Disable the third-party library check
-      },
-      telemetry = {
-        enable = false,  -- Disable telemetry data
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        typeCheckingMode = "standard",
       },
     },
   },
+})
+
+-- Lua language server
+vim.lsp.config('lua_ls', {
   capabilities = capabilities,
   on_attach = on_attach,
-}
-
-lspconfig.dartls.setup{
-    capabilities = capabilities,
-    on_attach = on_attach,
-}
-
-lspconfig.gopls.setup{
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        gopls = {
-            buildFlags = { "-tags=dev_test,dev,integration,unit" },
-        },
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
     },
-}
+  },
+})
+
+-- Dart language server
+vim.lsp.config('dartls', {
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+-- Go language server
+vim.lsp.config('gopls', {
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    gopls = {
+      buildFlags = { "-tags=dev_test,dev,integration,unit" },
+    },
+  },
+})
+
+-- Enable all of the above Language Server Protocol (LSP) configs
+vim.lsp.enable({ 'clangd', 'pyright', 'lua_ls', 'dartls', 'gopls' })
+
